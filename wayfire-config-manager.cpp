8eaabba6a165829536ab5e052a1789186758e860
Scott Moreau <oreaus@gmail.com>
2018-06-18 14:23:29 -0600

    Don't set precision for int option types

--- wayfire-config-manager.cpp
+++ wayfire-config-manager.cpp
@@ -161,7 +161,6 @@ get_plugin_data(Plugin *p, Option *opt, xmlDoc *doc, xmlNode * a_node)
 						o->type = OPTION_TYPE_INT;
 						o->data.min = DBL_MIN;
 						o->data.max = DBL_MAX;
-						o->data.precision = 0.1;
 					} else if (string((char *) prop) == "double") {
 						o->type = OPTION_TYPE_DOUBLE;
 						o->data.min = DBL_MIN;
