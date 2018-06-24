a70d5af3769c12190bf9290b856258ca806a6df0
Scott Moreau <oreaus@gmail.com>
2018-06-24 04:45:34 -0600

    Use -DBL_MAX instead of DBL_MIN

--- wayfire-config-manager.cpp
+++ wayfire-config-manager.cpp
@@ -160,11 +160,11 @@ get_plugin_data(Plugin *p, Option *opt, xmlDoc *doc, xmlNode * a_node)
                                         printf("%s\n", prop);
                                         if (string((char *) prop) == "int") {
                                                 o->type = OPTION_TYPE_INT;
-                                                o->data.min = DBL_MIN;
+                                                o->data.min = -DBL_MAX;
                                                 o->data.max = DBL_MAX;
                                         } else if (string((char *) prop) == "double") {
                                                 o->type = OPTION_TYPE_DOUBLE;
-                                                o->data.min = DBL_MIN;
+                                                o->data.min = -DBL_MAX;
                                                 o->data.max = DBL_MAX;
                                                 o->data.precision = 0.1;
                                         } else if (string((char *) prop) == "bool") {
