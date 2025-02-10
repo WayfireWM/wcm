4eea9ce7c8c347770884cd942123c3d526da8fb1
DMJC <jcarthew@gmail.com>
2025-02-10 10:39:10 +1030

    Patch to add cli option for launching wcm directly to plugins.
    * Patch to add cli option for launching wcm directly to plugins. For desktop integrations.
    
    * Fixes #93.

--- src/wcm.cpp
+++ src/wcm.cpp
@@ -1309,6 +1309,11 @@ WCM::WCM(Glib::RefPtr<Gtk::Application> app)
         wf_shell_config_file = value;
         return true;
     }, "shell-config", 's', "wf-shell config file to use", "file");
+    app->add_main_option_entry([this] (const Glib::ustring &, const Glib::ustring & value, bool)
+    {
+        start_plugin = value;
+        return true;
+    }, "plugin", 'p', "plugin to open at launch, or none for default", "name");
 
     app->signal_startup().connect([this, app] ()
     {
@@ -1577,6 +1582,12 @@ void WCM::create_main_layout()
     global_layout.pack_start(left_stack, false, false);
     global_layout.pack_start(main_stack, true, true);
     window->add(global_layout);
+    if (!start_plugin.empty())
+    {
+        Plugin *launch_plugin = find_plugin_by_name(plugins, start_plugin);
+        std::cout << "Opening Plugin: " << start_plugin << std::endl;
+        this->open_page(launch_plugin);
+    }
 }
 
 void WCM::open_page(Plugin *plugin)
@@ -1733,6 +1744,23 @@ void update_compound_from_section(wf::config::compound_option_t *compound,
     }
 
     compound->set_value_untyped(value);
+}
+
+Plugin*WCM::find_plugin_by_name(std::vector<Plugin*> plugins, std::string search_name)
+{
+    auto it = std::find_if(plugins.begin(), plugins.end(),
+        [&search_name] (const Plugin *plugin)
+    {
+        return plugin->name == search_name; // Compare the plugin name
+    });
+
+    if (it != plugins.end())
+    {
+        return *it; // Return the found Plugin pointer
+    }
+
+    std::cout << "plugin not found, name invalid" << std::endl;
+    return nullptr; // Return nullptr if not found
 }
 
 /**
