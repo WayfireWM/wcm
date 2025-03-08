69a5aaf9fdcd5fae5e064bb8e61ae3af811b8f22
Scott Moreau <oreaus@gmail.com>
2025-03-08 11:44:24 -0700

    Make wcm single instance (#96)
    We have to set org.gtk. prefix for the application identifier or the app can
    be started multiple times. This commit also moves from .png to .svg icon for
    wcm. We install wcm.svg to two different locations so it can be found by apps
    like wf-panel and still have wcm find it where it expects.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    auto app = Gtk::Application::create();
+    auto app = Gtk::Application::create("org.gtk.wcm");
     std::unique_ptr<WCM> wcm = std::make_unique<WCM>(app);
     return app->run(argc, argv);
 }
